8739f8c1dc4d6a23de3457b759a605067d66c1a2
Kiru Park <kirumang@gmail.com>
2018-09-21 14:24:01 +0200

    pos
    pos

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -8,5 +8,15 @@ var addressPoints = [
     "Venice, Italy",
     45.4371908,
     12.3345898
+  ],
+  [
+    "Hwaseong, South Korea",
+    37.1994653,
+    126.8312632
+  ],
+  [
+    "Seoul, South Korea",
+    37.5666791,
+    126.9782914
   ]
 ];
