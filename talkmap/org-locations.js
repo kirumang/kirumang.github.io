e8b511e01c6a860e2bda132375a94af5a5b7e7c5
Kiru Park <kirumang@gmail.com>
2018-09-21 15:00:19 +0200

    bb
    bb

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,8 @@
 var addressPoints = [
   [
-    "Vienna, Austria",
-    48.2083537,
-    16.3725042
+    "Hwaseong, South Korea",
+    37.1994653,
+    126.8312632
   ],
   [
     "Venice, Italy",
@@ -10,13 +10,18 @@ var addressPoints = [
     12.3345898
   ],
   [
-    "Hwaseong, South Korea",
-    37.1994653,
-    126.8312632
-  ],
-  [
     "Seoul, South Korea",
     37.5666791,
     126.9782914
+  ],
+  [
+    "Vienna, Austria",
+    48.2083537,
+    16.3725042
+  ],
+  [
+    "Kyoto, Japan",
+    35.0231321,
+    135.7634074
   ]
 ];
